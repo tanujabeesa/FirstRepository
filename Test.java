b1a4f81d2bd19f9ce26b0e3da67079551c519d45
tanujabeesa <135091331+tanujabeesa@users.noreply.github.com>
2023-05-31 09:35:34 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,23 @@
+ class Another {
+    public static void main(String[] args){
+        System.out.println("Hello Java");
+    }
+}
+class Another2 {
+    public static void main(String[] args){
+        System.out.println("Hello Java2");
+    }
+}
+class Another3 {
+    public static void main(String[] args){
+        System.out.println("Hello Java3");
+    }
+}
+
+class Another4{
+    public static void main(String[] args){
+        System.out.println("Hello Java4");
+    }
+}
+
+
